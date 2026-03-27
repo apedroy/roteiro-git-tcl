405a7b62fc0e92b109279abbb979198cbd5dacb7
fernandesepilef <fernandes.felipehenri@gmail.com>
2026-03-27 20:37:41 -0300

    estrutura inicial completa

--- roteiro_shell_make/organizador.sh
+++ roteiro_shell_make/organizador.sh
@@ -2,9 +2,28 @@
 
 # verificar se pastas já existem, se não existem criar
 
-if [!-d "src"] then
+if [ ! -d "src" ]; then
     echo "A pasta "src" não existe. Criando agora..."
-    mkdir "$src"
+    mkdir -p "src"
 else
     echo "A pasta já existe."
 fi 
+
+for file in *.v; do
+    if [ -f "$file" ]; then
+
+        if [ -f "src/$file" ]; then
+            NOME_BASE="${file%.*}"
+            EXT="v"
+    
+            NOVO_NOME="${NOME_BASE}2.${EXT}"
+    
+            echo "Aviso: $file já existe em scr. Renomeado para $NOVO_NOME"
+            mv "$file" "src/$NOVO_NOME"
+
+        else
+            mv "$file" "src/"
+            echo "$file movido para scr"
+        fi
+    fi
+done
